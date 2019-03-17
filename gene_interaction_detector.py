612bc399ff7890e7870f2b8e631ad31390ae704c
Felix B. Kwizera <fbkwizera@ke.ibm.com>
2019-03-17 16:14:55 +0300

    Initial commit
    Added code to detect genes detected in a converted pdf document

---
+++ gene_interaction_detector.py
@@ -0,0 +1,43 @@
+import nltk
+from tika import parser
+import io
+
+# nltk.download('punkt') # perform on first run
+
+def pdf_to_txt(pdf_file_name, out_txt_file_name):
+    content = parser.from_file(pdf_file_name)
+    with open(out_txt_file_name, 'w') as file:
+        file.write(content['content'])
+
+
+# use this method to find which proteins have been mentioned in the paper
+def intersection(lst1, lst2):
+    # Use of hybrid method
+    temp = set(lst2)
+    lst3 = [value for value in lst1 if value in temp]
+    return lst3
+
+
+def main(gene_names_file, paper_file_name):
+    with open(gene_names_file, 'r') as gene_file:
+        content = ''.join(gene_file.readlines())
+
+        # create a regex tokenizer that captures whole words, words with hyphens, full commas and forward slashes
+        tokenizer = nltk.RegexpTokenizer(r'\w[\w-][\w.][\w/]+')
+
+        gene_names = tokenizer.tokenize(content)
+
+        paper_file = io.open(paper_file_name, 'rU', encoding='utf-8')
+        content = ''.join(paper_file.readlines())
+        paper_text = tokenizer.tokenize(content)
+
+        result = intersection(gene_names, paper_text)
+
+        print(result)
+
+
+if __name__ == "__main__":
+    gene_names_file = 'gene-names.txt'
+    paper_file_name = 'corpus/1.txt'
+
+    main(gene_names_file, paper_file_name)
